5973ae9b198d118e2667795294f20b1e2461b785
Dominika Góralczyk <dominika.goralczyk@euvic.pl>
2021-12-05 21:35:17 +0100

    Dodanie nowych komponentów. Utworzenie widoku logowania

---
+++ Lib.WebUI/Features/Auth/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lib.WebUI.Features.Auth
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            return View("Login/Login");
+        }
+    }
+}
